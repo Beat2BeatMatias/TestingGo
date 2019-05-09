2f5e14148c0b02a8c08ddde048375257acf957e8
Matias Mirko <mmffalkiewicz@gmail.com>
2019-05-09 10:09:14 -0300

    "nada"

--- mio/acumulador.go
+++ mio/acumulador.go
@@ -4,12 +4,12 @@ import (
 	"errors"
 )
 
-func Acumular (lista []int) error{
+func Acumular(lista []int) error {
 	if lista == nil {
 		return errors.New("La lista está vacía")
 	}
-	for i:=1; i<len(lista); i++{
-		lista[i]=lista[i-1] + lista[i]
+	for i := 1; i < len(lista); i++ {
+		lista[i] = lista[i-1] + lista[i]
 	}
 	return nil
 }
